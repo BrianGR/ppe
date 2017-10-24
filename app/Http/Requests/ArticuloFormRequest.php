a6e870b78db5821d338e3e6b4cfa7b63338b8082
BrianGR <brian.grandon3@gmail.com>
2017-10-24 19:18:11 -0300

    kn
    hola

---
+++ app/Http/Requests/ArticuloFormRequest.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace sisventas\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Articulo;
+
+
+class ArticuloFormRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+             'idcategoria' =>'required',
+             'impuesto' =>'required',
+             'codigo' =>'required |max:50|unique:articulo',
+             'nombre'=>'required |max:60',
+             'stock'=>'required |numeric',
+             'descripccion'=>'max:500',
+             'imagen'=>'mimes:jpeg,jpg,bmp,png'
+        ];
+    }
+}
